ac20579fbde21e70c852302df3e97b8314e4c287
Muhammad Farhan <muhanmedan06@gmail.com>
2025-12-04 17:44:18 +0700

    Refactor layout structure for improved navigation and profile editing; enhance user display with Font Awesome icon

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,7 +6,8 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="max-w-xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <h3 class="text-2xl font-semibold">Update Profile Information</h3>
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
@@ -19,11 +20,11 @@
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            {{-- <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 </x-app-layout>
